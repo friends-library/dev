e4065700587f67b4f9098b662f9d47fd4f980c83
Jared Henderson <jaredh159@users.noreply.github.com>
2023-02-28 17:07:44 -0500

    xnative: test audio playback

--- apps/xnative/index.js
+++ apps/xnative/index.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+import PlaybackService from './service';
 import App from './App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => PlaybackService);
