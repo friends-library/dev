502edbd60b707138b67bfa6cca5f7b13fa9dc655
Jared Henderson <jaredh159@users.noreply.github.com>
2023-03-01 15:18:06 -0500

    xnative: handle dynamic env with set-lang.ts

--- apps/xnative/App.tsx
+++ apps/xnative/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import { t, setLocale } from '@friends-library/locale';
+import { APP_NAME } from './env';
 
 function App(): JSX.Element {
   setLocale(`es`);
   return (
     <SafeAreaView>
-      <Text>howdy {t`Download`}</Text>
+      <Text>
+        {APP_NAME} howdy {t`Download`}
+      </Text>
     </SafeAreaView>
   );
 }
