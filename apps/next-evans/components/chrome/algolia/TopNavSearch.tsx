33f5e3380aa516ccfd9d72bfe93c84b654afad46
Jared Henderson <jaredh159@users.noreply.github.com>
2023-12-06 12:37:01 -0500

    next-evans: fix search not dismissing when clicking result

--- apps/next-evans/components/chrome/algolia/TopNavSearch.tsx
+++ apps/next-evans/components/chrome/algolia/TopNavSearch.tsx
@@ -21,28 +21,32 @@ const TopNavSearch: React.FC<Props> = ({ className, searching, setSearching }) =
   }, [searching]);
 
   return (
-    <FocusLock
-      disabled={!searching}
-      returnFocus
-      autoFocus={false}
-      className={cx(
-        className,
-        `TopNavSearch flex-col justify-center items-end relative`,
-        searching &&
-          `flex-grow pl-4 sm:pl-8 md:pl-16 [&_form]:w-full [&_form]:max-w-[550px] [&_input]:w-full [&_input]:max-w-[550px]`,
-        !searching && `flex-grow-0`,
-      )}
-      lockProps={{ onClick: () => !searching && setSearching(true) }}
-    >
-      <InstantSearch indexName={`${LANG}_docs`} searchClient={searchClient}>
-        <Search searching={searching} defaultRefinement="" />
-        {searching && <DropdownSearchResults />}
-      </InstantSearch>
-    </FocusLock>
+    <DismissSearch.Provider value={() => setSearching(false)}>
+      <FocusLock
+        disabled={!searching}
+        returnFocus
+        autoFocus={false}
+        className={cx(
+          className,
+          `TopNavSearch flex-col justify-center items-end relative`,
+          searching &&
+            `flex-grow pl-4 sm:pl-8 md:pl-16 [&_form]:w-full [&_form]:max-w-[550px] [&_input]:w-full [&_input]:max-w-[550px]`,
+          !searching && `flex-grow-0`,
+        )}
+        lockProps={{ onClick: () => !searching && setSearching(true) }}
+      >
+        <InstantSearch indexName={`${LANG}_docs`} searchClient={searchClient}>
+          <Search searching={searching} defaultRefinement="" />
+          {searching && <DropdownSearchResults />}
+        </InstantSearch>
+      </FocusLock>
+    </DismissSearch.Provider>
   );
 };
 
 export default TopNavSearch;
+
+export const DismissSearch = React.createContext<() => unknown>(() => {});
 
 function focusInput(): void {
   const input: HTMLInputElement | null = document.querySelector(
