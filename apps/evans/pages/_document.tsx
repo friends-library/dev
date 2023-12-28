a499db17511b75407a1229447946138481d05dd4
Jared Henderson <jaredh159@users.noreply.github.com>
2023-12-28 11:41:28 -0500

    evans: restore favicon and apple-touch-icons forgotten in gatsby->next conversion

--- apps/evans/pages/_document.tsx
+++ apps/evans/pages/_document.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
 import { NextBgStaticCss } from 'next-bg-image';
+import { LANG } from '@/lib/env';
 
 const Document: React.FC = () => (
   <Html lang="en">
     <Head>
       <NextBgStaticCss />
+      <link
+        rel="icon"
+        href={`/images/icons/favicon-32x32.${LANG}.png`}
+        type="image/png"
+      />
+      {[48, 72, 96, 144, 192, 256, 384, 512].map((size) => (
+        <link
+          key={size}
+          rel="apple-touch-icon"
+          sizes={`${size}x${size}`}
+          href={`/images/icons/icon-${size}x${size}.${LANG}.png`}
+        />
+      ))}
     </Head>
     <body>
       <Main />
