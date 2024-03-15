6e1732c989a76a450e276a1e4a05a8e4e2466847
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-15 11:26:42 -0400

    api: add pairql route for ingesting native app errors

---
+++ apps/api/Tests/AppTests/PairQL/NativeDomainTests.swift
@@ -0,0 +1,41 @@
+import DuetSQL
+import XCTest
+import XExpect
+
+@testable import App
+
+final class NativeDomainTests: AppTestCase {
+  func testReportNativeAppError() async throws {
+    let installId = UUID().lowercased
+    let output = try await ReportError.resolve(
+      with: .init(
+        buildSemver: "3.3.3",
+        buildNumber: 101,
+        lang: .en,
+        detail: "printer on fire",
+        platform: "ios",
+        installId: installId,
+        errorMessage: "whoops",
+        errorStack: nil
+      ),
+      in: .init(requestId: "someid".random)
+    )
+
+    expect(output).toEqual(.success)
+
+    let retrieved = try await NativeAppError.query()
+      .where(.installId == installId)
+      .first()
+
+    expect(retrieved.buildSemver).toEqual("3.3.3")
+    expect(retrieved.buildNumber).toEqual(101)
+    expect(retrieved.lang).toEqual(.en)
+    expect(retrieved.detail).toEqual("printer on fire")
+    expect(retrieved.platform).toEqual("ios")
+    expect(retrieved.installId).toEqual(installId)
+    expect(retrieved.errorMessage).toEqual("whoops")
+    expect(retrieved.errorStack).toBeNil()
+
+    expect(sent.slacks).toHaveCount(1)
+  }
+}
