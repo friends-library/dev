43274202e1770be19a893461bcee779988d6cc91
Jared Henderson <jaredh159@users.noreply.github.com>
2024-04-23 12:28:36 -0400

    wip: another mega query: dev GetAudios

--- apps/api/Sources/App/PairQL/Dev/GetAudios.swift
+++ apps/api/Sources/App/PairQL/Dev/GetAudios.swift
@@ -57,55 +57,49 @@ struct GetAudios: Pair {
 extension GetAudios: NoInputResolver {
   static func resolve(in context: AuthedContext) async throws -> Output {
     try context.verify(Self.auth)
-    fatalError("mega query")
-    // let audios = try await App.Audio.query().all()
-    // return try await audios.concurrentMap { audio in
-    //   var audio = audio
-    //   let parts = try await audio.parts()
-    //   var edition = try await audio.edition()
-    //   var document = try await edition.document()
-    //   let friend = try await document.friend()
-    //   return .init(
-    //     id: audio.id,
-    //     isIncomplete: audio.isIncomplete,
-    //     m4bSizeHq: audio.m4bSizeHq.rawValue,
-    //     m4bSizeLq: audio.m4bSizeLq.rawValue,
-    //     mp3ZipSizeHq: audio.mp3ZipSizeHq.rawValue,
-    //     mp3ZipSizeLq: audio.mp3ZipSizeLq.rawValue,
-    //     reader: audio.reader,
-    //     parts: parts.map { part in
-    //       .init(
-    //         id: part.id,
-    //         chapters: Array(part.chapters),
-    //         durationInSeconds: part.duration.rawValue,
-    //         title: part.title,
-    //         order: part.order,
-    //         mp3SizeHq: part.mp3SizeHq.rawValue,
-    //         mp3SizeLq: part.mp3SizeLq.rawValue
-    //       )
-    //     },
-    //     edition: .init(
-    //       id: edition.id,
-    //       path: edition.directoryPath,
-    //       type: edition.type,
-    //       coverImagePath: edition.images.square.w1400.path
-    //     ),
-    //     document: .init(
-    //       filename: document.filename,
-    //       title: document.title,
-    //       slug: document.slug,
-    //       description: document.description,
-    //       path: document.directoryPath,
-    //       tags: []
-    //     ),
-    //     friend: .init(
-    //       lang: friend.lang,
-    //       name: friend.name,
-    //       slug: friend.slug,
-    //       alphabeticalName: friend.alphabeticalName,
-    //       isCompilations: friend.isCompilations
-    //     )
-    //   )
-    // }
+    let audios = try await JoinedEntities.shared.audios()
+    return audios.map { audio in
+      .init(
+        id: audio.id,
+        isIncomplete: audio.isIncomplete,
+        m4bSizeHq: audio.m4bSizeHq.rawValue,
+        m4bSizeLq: audio.m4bSizeLq.rawValue,
+        mp3ZipSizeHq: audio.mp3ZipSizeHq.rawValue,
+        mp3ZipSizeLq: audio.mp3ZipSizeLq.rawValue,
+        reader: audio.reader,
+        parts: audio.parts.map { part in
+          .init(
+            id: part.id,
+            chapters: Array(part.chapters),
+            durationInSeconds: part.duration.rawValue,
+            title: part.title,
+            order: part.order,
+            mp3SizeHq: part.mp3SizeHq.rawValue,
+            mp3SizeLq: part.mp3SizeLq.rawValue
+          )
+        },
+        edition: .init(
+          id: audio.edition.id,
+          path: audio.edition.directoryPath,
+          type: audio.edition.type,
+          coverImagePath: audio.edition.images.square.w1400.path
+        ),
+        document: .init(
+          filename: audio.edition.document.filename,
+          title: audio.edition.document.title,
+          slug: audio.edition.document.slug,
+          description: audio.edition.document.description,
+          path: audio.edition.document.directoryPath,
+          tags: audio.edition.document.tags
+        ),
+        friend: .init(
+          lang: audio.edition.document.friend.lang,
+          name: audio.edition.document.friend.name,
+          slug: audio.edition.document.friend.slug,
+          alphabeticalName: audio.edition.document.friend.alphabeticalName,
+          isCompilations: audio.edition.document.friend.isCompilations
+        )
+      )
+    }
   }
 }
