20fd4825cd7fa935210612a4d65be8f8ef577d1c
Jared Henderson <jaredh159@users.noreply.github.com>
2024-04-24 12:50:27 -0400

    wip: final mega query vanquished: VerifyCloudAssets

--- apps/api/Sources/App/Services/VerifyCloudAssets.swift
+++ apps/api/Sources/App/Services/VerifyCloudAssets.swift
@@ -3,33 +3,31 @@ import Vapor
 
 public struct VerifyCloudAssets: AsyncScheduledJob {
   public func run(context: QueueContext) async throws {
-    fatalError("mega query")
-    // let editionImpressions = try await Current.db.query(EditionImpression.self).all()
-    // let audios = try await Current.db.query(Audio.self).all()
-    // let audioParts = try await Current.db.query(AudioPart.self).all()
+    let editionImpressions = try await JoinedEntities.shared.editionImpressions()
+    let audios = try await JoinedEntities.shared.audios()
+    let audioParts = audios.flatMap(\.parts)
 
-    // let impressionFiles = try await editionImpressions.concurrentMap { impression in
-    //   let files = try await impression.files()
-    //   return files.all.map(\.sourceUrl)
-    // }.flatMap { $0 }
+    let impressionFiles = editionImpressions
+      .map { $0.files.all.map(\.sourceUrl) }
+      .flatMap { $0 }
 
-    // let allUrls = impressionFiles
-    //   + audios.flatMap(\.files.all).map(\.sourceUrl)
-    //   + audioParts.flatMap(\.mp3File.all).map(\.sourceUrl)
+    let allUrls = impressionFiles
+      + audios.flatMap(\.files.all).map(\.sourceUrl)
+      + audioParts.flatMap(\.mp3File.all).map(\.sourceUrl)
 
-    // let chunks = allUrls
-    //   .filter { !$0.absoluteString.hasSuffix(".rss") } // ignore podcasts
-    //   .map { URI(string: $0.absoluteString) }
-    //   .chunked(into: Env.mode == .dev ? 50 : 25)
+    let chunks = allUrls
+      .filter { !$0.absoluteString.hasSuffix(".rss") } // ignore podcasts
+      .map { URI(string: $0.absoluteString) }
+      .chunked(into: Env.mode == .dev ? 50 : 25)
 
-    // var missing: [String] = []
-    // for uris in chunks {
-    //   missing.append(contentsOf: try await verify(chunk: uris, in: context))
-    // }
+    var missing: [String] = []
+    for uris in chunks {
+      missing.append(contentsOf: try await verify(chunk: uris, in: context))
+    }
 
-    // guard !missing.isEmpty else { return }
-    // let list = "- " + missing.joined(separator: "\n- ")
-    // await slackError("Missing \(missing.count)/\(allUrls.count) cloud assets:\n\n\(list)")
+    guard !missing.isEmpty else { return }
+    let list = "- " + missing.joined(separator: "\n- ")
+    await slackError("Missing \(missing.count)/\(allUrls.count) cloud assets:\n\n\(list)")
   }
 
   func verify(chunk uris: [URI], in context: QueueContext) async throws -> [String] {
