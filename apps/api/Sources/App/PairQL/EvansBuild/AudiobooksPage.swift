0055b2b9d8b9710c104aa14d1b6f76c14dced705
Jared Henderson <jaredh159@users.noreply.github.com>
2024-04-24 12:27:43 -0400

    wip: another mega query: AudiobooksPage

--- apps/api/Sources/App/PairQL/EvansBuild/AudiobooksPage.swift
+++ apps/api/Sources/App/PairQL/EvansBuild/AudiobooksPage.swift
@@ -29,33 +29,32 @@ struct AudiobooksPage: Pair {
 extension AudiobooksPage: Resolver {
   static func resolve(with input: Input, in context: AuthedContext) async throws -> Output {
     try context.verify(Self.auth)
-    fatalError("mega query")
-    // let audiobooks = try await Audio.query().all()
-    // return try audiobooks.compactMap { audiobook in
-    //   let edition = audiobook.edition.require()
-    //   let document = edition.document.require()
-    //   let friend = document.friend.require()
-    //   let parts = audiobook.parts.require()
-    //   guard friend.lang == input,
-    //         !edition.isDraft,
-    //         !parts.isEmpty,
-    //         edition.impression.require() != nil else {
-    //     return nil
-    //   }
-    //   return .init(
-    //     slug: document.slug,
-    //     title: document.title,
-    //     htmlShortTitle: document.htmlShortTitle,
-    //     editionType: edition.type,
-    //     isbn: try expect(edition.isbn.require()).code,
-    //     isCompilation: friend.isCompilations,
-    //     friendName: friend.name,
-    //     friendSlug: friend.slug,
-    //     friendGender: friend.gender,
-    //     duration: audiobook.humanDurationClock,
-    //     shortDescription: document.partialDescription,
-    //     createdAt: audiobook.createdAt
-    //   )
-    // }
+    let audiobooks = try await JoinedEntities.shared.audios()
+    return try audiobooks.compactMap { audiobook in
+      let edition = audiobook.edition
+      let document = edition.document
+      let friend = document.friend
+      let parts = audiobook.parts
+      guard friend.lang == input,
+            !edition.isDraft,
+            !parts.isEmpty,
+            edition.impression != nil else {
+        return nil
+      }
+      return .init(
+        slug: document.slug,
+        title: document.title,
+        htmlShortTitle: document.htmlShortTitle,
+        editionType: edition.type,
+        isbn: try expect(edition.isbn).code,
+        isCompilation: friend.isCompilations,
+        friendName: friend.name,
+        friendSlug: friend.slug,
+        friendGender: friend.gender,
+        duration: audiobook.humanDurationClock,
+        shortDescription: document.partialDescription,
+        createdAt: audiobook.createdAt
+      )
+    }
   }
 }
