4d4db7c008bcb1e0fb5252c67192999db0518d50
Jared Henderson <jaredh159@users.noreply.github.com>
2024-09-17 12:19:30 -0400

    actions-ts-pack: update meta action

--- actions/ts-pack/actions/meta/index.ts
+++ actions/ts-pack/actions/meta/index.ts
@@ -3,25 +3,26 @@ import { latestCommitSha } from '../helpers';
 import * as pr from '../pull-requests';
 
 async function main(): Promise<void> {
-  const sha = latestCommitSha() || ``;
-  const shortSha = sha.substr(0, 8);
+  const sha = latestCommitSha();
   if (!sha) {
-    core.warning(`Unable to find latest commit sha`);
+    core.setFailed(`Unable to find latest commit sha`);
+    return;
   }
 
+  const shortSha = sha.substr(0, 8);
   const prData = await pr.data();
-  const prNum = prData ? prData.number : pr.number() || ``;
-  const prTitle = prData ? prData.title : ``;
+  const prNum: number | null = prData ? prData.number : (await pr.number()) || null;
+  const prTitle: string | null = prData?.title ?? null;
 
   core.setOutput(`latest_commit_sha`, sha);
   core.setOutput(`latest_commit_sha_short`, shortSha);
-  core.setOutput(`pull_request_number`, prNum);
-  core.setOutput(`pull_request_title`, prTitle);
+  core.setOutput(`pull_request_number`, prNum ?? ``);
+  core.setOutput(`pull_request_title`, prTitle ?? ``);
 
-  core.info(`Output \`latest_commit_sha\` set to ${sha || `<empty>`}`);
-  core.info(`Output \`latest_commit_sha_short\` set to ${shortSha || `<empty>`}`);
-  core.info(`Output \`pull_request_number\` set to ${prNum || `<empty>`}`);
-  core.info(`Output \`pull_request_title\` set to ${`"${prTitle}"` || `<empty>`}`);
+  core.info(`Output \`latest_commit_sha\` set to ${sha}`);
+  core.info(`Output \`latest_commit_sha_short\` set to ${shortSha}`);
+  core.info(`Output \`pull_request_number\` set to ${prNum ?? `(null)`}`);
+  core.info(`Output \`pull_request_title\` set to ${prTitle ?? `(null)`}`);
 }
 
 main();
